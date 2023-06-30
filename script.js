a502a2d12873e9d47f274d4ae79ab7aa72334443
Priyam8878 <112754436+Priyam8878@users.noreply.github.com>
2023-06-30 09:35:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	const keys = Object.keys(sampleObject);
+	
+ for (let i=0 ;i<keys.length;i++){
+    if(keys[i]==key){
+        return "true"
+    }else{
+        return "false"
+    }
+  }
 }
 
 // Do not change the code below
